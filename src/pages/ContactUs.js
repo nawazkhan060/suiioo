faf1fac021981678eb9608b51d8a097d372373c2
NAWAZZ KHAN <kagamergamero@gmail.com>
2025-11-08 21:36:14 +0530

    Update ContactUs.js

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -240,7 +240,7 @@ const ContactUs = () => {
                     required
                     rows="6"
                     className="w-full px-4 py-3 border border-slate-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition-colors resize-none"
-                    placeholder="Please provide details about your inquiry..."
+                    placeholder="Please provide details about your inquiry.."
                   />
                 </div>
                 
@@ -269,5 +269,6 @@ const ContactUs = () => {
     </div>
   );
 };
+
 
 export default ContactUs;
